43688a4de554a2a661e7e41a910cbd1373c188af
ewsoldan <ewsoldan@gmail.com>
2020-07-12 09:24:21 +0200

    dodane dependencies

--- src/test/java/DevToTests.java
+++ src/test/java/DevToTests.java
@@ -1,11 +1,19 @@
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class DevToTests {
 
     private WebDriver driver;
-    public String baseURL;
+    public String baseURL = "https://dev.to/";
 
 
     @Before
@@ -14,6 +22,29 @@ public class DevToTests {
 
         driver = new ChromeDriver();
         driver.get(baseURL);
+
+    }
+
+    @Test
+
+    public void Test1(){
+
+        String currentURL = driver.getCurrentUrl();
+        assertTrue("The current url isn't dev.to",baseURL.equals(currentURL));
+
+
+    }
+
+    @Test
+
+    public void Test2() throws InterruptedException {
+        WebElement week = driver.findElement(By.xpath("//*[@id=\"on-page-nav-controls\"]/div/nav/a[2]"));
+        week.click();
+       // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        String currentURL = driver.getCurrentUrl();
+        assertTrue("The current url isn't dev.to",currentURL.equals("https://dev.to/top/week/"));
+
+        //assertEquals("Czy przekierowalo na strone https://dev.to/top/week", "https://dev.to/top/week", currentURL);
 
     }
 }
